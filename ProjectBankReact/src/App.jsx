3a5f04718b450ae98e7fdf4167629122e06292f3
DESKTOP-LK0VHNL\setsu <cristian.castillo.campos1993@gmail.com>
2025-06-27 18:51:52 -0600

    Se agregan los componentes y un archivo test borrar

--- ProjectBankReact/src/App.jsx
+++ ProjectBankReact/src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 const App = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-500">Hello, World!</h1>
+      <h1 className="text-4xl font-bold text-red-500">Hello, World!</h1>
     </div>
   );
 };
